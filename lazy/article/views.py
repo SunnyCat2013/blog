b508fe290f0b49e952afab98b5c9d8e3e7405f5e
李振洋 <lizhenyang@raintai.com>
2016-06-11 19:45:56 +0800

    show blogs, next to edit blogs with MarkDown

--- lazy/article/views.py
+++ lazy/article/views.py
@@ -7,22 +7,23 @@ from article.models import Article
 def home(request):
     template = loader.get_template('index.html')
     #return HttpResponse("Hello Django, agian. ??")
-    return HttpResponse(template.render({}, request))
+    return_dict = {'blog_list': Article.objects.all()}
+    return HttpResponse(template.render(return_dict, request))
 
 
 def detail(request, args):
     # detect if args is well-format
 
     try:
-        article_id = int(args) - 1
+        article_id = int(args)
     except:
         return HttpResponse("Invalid Article Number...")
 
     article_list = Article.objects.all()
-    if article_id not in range(len(article_list)):
+    if article_id not in range(1, len(article_list)+1):
         return HttpResponse("Invalid Article Number...")
 
-    atc = Article.objects.all()[article_id]
+    atc = Article.objects.get(id=article_id)
     template = loader.get_template('show_atc.html')
     return_dict = {'title': atc.title, 'category': atc.category, 'date': atc.date_time, 'content': atc.content}
     return HttpResponse(template.render(return_dict, request))
